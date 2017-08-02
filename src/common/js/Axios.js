46cb08bd18e05c8e900f1bdc085e23be36a468b0
zaxlct <zaxlct@foxmail.com>
2017-08-02 16:23:58 +0800

    完善 ajaxFactory

--- src/common/js/Axios.js
+++ src/common/js/Axios.js
@@ -6,15 +6,13 @@ const Axios = axios.create({
   timeout: 5000,
 })
 
-//POST传参序列化(添加请求拦截器)
 Axios.interceptors.request.use(config => {
-  // 在发送请求之前做某件事
+  //POST传参序列化
   if(config.method  === 'post'){
     // 数据序列化成表单
-    const data = { config }
     const formData = new FormData()
-    Object.keys(data).forEach(key => formData.append(key, data[key]))
-    config.data = qs.stringify(formData)
+    Object.keys(config.data).forEach(key => formData.append(key, config.data[key]))
+    config.data = formData
   }
 
   return config
@@ -23,22 +21,21 @@ Axios.interceptors.request.use(config => {
   return Promise.reject(error)
 })
 
-//
-// Axios.interceptors.response.use(res => {
-//   return res.data
-// },error => {
-//   if(!error.response) return Promise.reject(error)
-//   if(error.response.status === 401) {
-//     // 未登录 or  token 过期，跳转到登录页面
-//     localStorage.token = ''
-//     location.href = '/login'
-//   } else if (error.response.status === 400) {
-//     const { data } = error.response
-//     const errorMsg = Object.keys(data).reduce((str, key) => str + data[key][0] + '      ', '')
-//     Message.error(errorMsg.trim())
-//   }
-//   return Promise.reject(error.response.data)
-// })
+
+Axios.interceptors.response.use(res => {
+  const { data } = res
+  if(data.success) {
+    return data
+  } else if (data.errCode === '00002' && !data.success) {
+    router.push('/login')
+  } else {
+    Message.error(data.errMsg)
+    return Promise.reject(data.errMsg)
+  }
+},error => {
+  Message.error('网络错误，请检查网络再试！')
+  return Promise.reject(error)
+})
 
 export default {
   install(Vue) {
