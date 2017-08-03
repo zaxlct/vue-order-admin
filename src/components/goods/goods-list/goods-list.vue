581de8f0a28c6ae698951cb51814172f8eec9c11
zaxlct <zaxlct@foxmail.com>
2017-08-03 09:43:35 +0800

    商品列表页

---
+++ src/components/goods/goods-list/goods-list.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+  <goods-menu></goods-menu>
+</div>
+</template>
+<script>
+  import GoodsMenu from 'base/goods-menu/goods-menu'
+  export default {
+    components: {
+      GoodsMenu,
+    },
+  }
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 const OrderList = resolve => import('order/order-list/order-list').then(module => resolve(module))
 const Login = resolve => import('login/login').then(module => resolve(module))
+const GoodsList = resolve => import('goods/goods-list/goods-list').then(module => resolve(module))
 
 const routers = [
   {
@@ -21,7 +22,15 @@ const routers = [
     meta: {
       title: '登录'
     },
-  }
+  },
+
+  {
+    path: '/goods-list/:order_id',
+    component: GoodsList,
+    meta: {
+      title: '商品列表'
+    },
+  },
 ]
 
 export default routers
