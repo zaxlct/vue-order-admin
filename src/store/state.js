a702e5745ca195db1c0ae5277b0269857464e569
zaxlct <zaxlct@foxmail.com>
2017-08-07 22:34:47 +0800

    记录左侧商品分类的层级 id

--- src/store/state.js
+++ src/store/state.js
@@ -7,6 +7,10 @@ const state = {
     page_size: 6,
     search_key: '',
     order_id: '',
+    // current_id 三个参数只是记录商品菜单的层级，请求 API 时不需要这三个参数
+    current_id_a: 0,
+    current_id_b: 0,
+    current_id_c: 0,
   },
   goodsList: {},
   orderDetail: [],
