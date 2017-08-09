88d86e5698f0fb8e113fafc3ee02b07db63c1b96
zaxlct <zaxlct@foxmail.com>
2017-08-09 09:20:45 +0800

    黑科技过滤对象里的 key

--- src/store/actions.js
+++ src/store/actions.js
@@ -4,27 +4,19 @@ import { Axios } from "common/js/Axios"
 export const fetchGoodsList = function({state, commit}, params) {
   commit(types.CHANGE_FETCH_GOODS_LIST_PARAMS, params)
   // current_id 三个参数只是记录商品菜单的层级，请求 API 时不需要这三个参数
-  // 过滤一下这三个参数
-  const {
-    menu_id,
-    level,
-    goods_color,
-    page_index,
-    page_size,
-    search_key,
-    order_id,
-  } = state.fetchGoodsListParams
-  const paramsFilter = {
-    menu_id,
-    level,
-    goods_color,
-    page_index,
-    page_size,
-    search_key,
-    order_id,
-  }
+  // 列举需要的参数
+  const paramsList = [
+    'menu_id',
+    'level',
+    'goods_color',
+    'page_index',
+    'page_size',
+    'search_key',
+    'order_id',
+  ]
+  const newParamsStr = JSON.stringify(state.fetchGoodsListParams, paramsList)
 
-  Axios.get('goods/goods_list', {params: paramsFilter}).then(res => {
+  Axios.get('goods/goods_list', {params: JSON.parse(newParamsStr)}).then(res => {
     if(!res) return
     if(!res.data) {
       commit(types.UPDATE_GOODS_LIST, {})
